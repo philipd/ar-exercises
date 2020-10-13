27e37f67352a69afd9a859abdc79be24108c41a8
Philip Dickau <philip.hd@protonmail.com>
2020-10-13 10:39:19 -0600

    Complete activity 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total Annual Revenue: #{Store.sum(:annual_revenue)}"
+puts "Average Annual Revenue: #{Store.average(:annual_revenue)}"
+puts "Number of stores generating more than $1M: #{Store.where('annual_revenue <= 1000000').count}"
