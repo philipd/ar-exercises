b6c425b643a9a2a5209e407d118f9755f77eb334
Philip Dickau <philip.hd@protonmail.com>
2020-10-13 12:06:23 -0600

    Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+puts "Number of stores: #{Store.count}"
+
+@store1 = Store.find_by(name: "Gastown")
+pp @store1
+@store1.employees.create(first_name: "Philip", last_name: "Dickau", hourly_rate: 80)
+@store1.employees.create(first_name: "Parker", last_name: "Thiessen", hourly_rate: 60)
+@store1.employees.create(first_name: "Max", last_name: "Uhlich", hourly_rate: 70)
+
+@store2 = Store.find_by(name: "Richmond")
+@store2.employees.create(first_name: "Amy", last_name: "McLean", hourly_rate: 60)
+@store2.employees.create(first_name: "Robyn", last_name: "Taylor-Neu", hourly_rate: 80)
+@store2.employees.create(first_name: "Mitchell", last_name: "Hopkins", hourly_rate: 70)
+
+puts Employee.all
